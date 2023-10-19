f4a9d4edf844fcf9b90cae5095284f9547468352
Michael Chinn <michael@chinn.me>
2023-10-19 04:12:11 +0000

    chntpw: add 1.0.1 bottle.

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -5,6 +5,12 @@ class Chntpw < Formula
   sha256 "886d97ed85a0d9c34e1af3e5c861f48fa6c0161530059f553f77168639d3f3a2"
   license all_of: ["LGPL-2.1-only", "GPL-2.0-only"]
 
+  bottle do
+    root_url "https://ghcr.io/v2/mechinn/tap"
+    sha256 cellar: :any,                 ventura:      "058ac318fefa2c8565aab2819b80118447dcf061eeba0360af1ca18ffb62c161"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "eec5c6f1ee7e7d73d2d2dcd8e09d3acde7b7ca9998ce6838d915935dbb9ef944"
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc" => :build
   depends_on "openssl@3"
